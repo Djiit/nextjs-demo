d50696d470c50b7fffc12bb7b6ddd32208cb16e3
Julien Tanay <julien.tanay@consensys.net>
2020-06-18 15:28:32 +0200

    tada

---
+++ pages/post/[id].tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function About() {
+  return (
+    <div className="container">
+      <Head>
+        <title>COP Engineering</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    <div>
+      tada
+        </div> 
+    </div>
+  )
+}
